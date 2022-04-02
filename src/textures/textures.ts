ce22569250d1fef411114407aff3efe67453b4a7
Jerzy Rutkowski <jerzyx11@gmail.com>
2022-04-02 17:29:55 +0200

    slowly adding textures

---
+++ src/textures/textures.ts
@@ -0,0 +1,15 @@
+// grass
+const grass_t = new Image();
+grass_t.src = './assets/graphics/grass1.png'
+// sand
+const sand_t = new Image();
+sand_t.src = './assets/graphics/sand1.png'
+// water
+const water = new Image();
+water.src = './assets/graphics/water/water.png'
+
+const textures = {
+    0: grass_t,
+    1: sand_t,
+    2: water
+}
