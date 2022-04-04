1b0e38d873a884204bb21d45372f00b6700629ee
teaver <jerzyx11@gmail.com>
2022-04-04 21:57:51 +0200

    working on objectLayer

---
+++ src/layers/ObjectLayer.ts
@@ -0,0 +1,23 @@
+import TextureLayer from "./TextureLayer";
+
+export default class ObjectLayer extends TextureLayer {
+
+    // obj:number[][];
+
+
+    constructor(_domCtx: CanvasRenderingContext2D, ) {
+        super(_domCtx)
+        // this.obj = [][];
+        
+    } 
+
+    private loadObjects(_obj:number[][]){
+        for (let i = 0; i < _obj.length; i++) {
+            for (let j = 0; j < _obj.length; j++) {
+                   
+            }
+        }
+    }
+
+}
+
