c71f44ea51eccec146d440d7de8da90cd9e572e1
Jerzy Rutkowski <jerzyx11@gmail.com>
2022-04-18 03:47:35 +0200

    objGrid problem

--- src/layers/ObjectLayer.ts
+++ src/layers/ObjectLayer.ts
@@ -5,21 +5,24 @@ import Atlas from "./Atlas";
 export default class ObjectLayer extends TextureLayer {
 
     textureAtlas:Atlas
+    objGrid:ObjectGrid
 
-    constructor(_domCtx: CanvasRenderingContext2D, _textureAtlas:Atlas) {
+    constructor(_domCtx: CanvasRenderingContext2D, _textureAtlas:Atlas, _objGrid:ObjectGrid) {
         super(_domCtx)
         this.textureAtlas = _textureAtlas
+        this.objGrid = _objGrid
         this.canvas.width = 1920
         this.canvas.height = 1920
     } 
 
-    public loadObjects(_obj: ObjectGrid){
+    public loadObjects(){
         for (let i = 0; i < this.canvas.width/this.blockSize; i++) {
             for (let j = 0; j < this.canvas.height/this.blockSize; j++) {
-                if(_obj.grid[i][j] !== 0){ //temp
+                if(this.objGrid.grid[i][j] !== 0){ //temp
                     this.ctx.drawImage(this.textureAtlas.texture, 
-                        this.textureAtlas.cords[_obj.grid[i][j]].x, 
-                        this.textureAtlas.cords[_obj.grid[i][j]].y, 
+                        // thisobjGrid
+                        this.textureAtlas.cords[this.objGrid.grid[i][j]].x, 
+                        this.textureAtlas.cords[this.objGrid.grid[i][j]].y, 
                         this.blockSize, 
                         this.blockSize, 
                         i*this.blockSize, 
